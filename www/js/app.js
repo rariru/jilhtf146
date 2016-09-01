129946ca3b6e9d526efc6b49b5c9edfe641d781b
hamzahppa <hamzahibnabdullah@gmail.com>
2016-09-01 18:40:12 +0700

    Feature Promo

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'ngCordova', 'ngStorage', 'ionic-ratings', 'ionicLazyLoad'])
 
 .constant('config', {
-  analytics: 'UA-XXXXXXXX-X'
+  analytics: ''
 })
 
 .run(function($ionicPlatform, config) {
